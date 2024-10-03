c3245f491a061c0fe11d54166d4d3db15b49a741
Nils <nilekl0117@skola.goteborg.se>
2024-10-03 15:36:58 +0200

    fixed git push

--- git_push.py
+++ git_push.py
@@ -1,5 +1,6 @@
 import subprocess
 
+subprocess.call(['git','-C','/home/nils/Test-och-Automatisering-Data','pull'])
 subprocess.call(['git','-C','/home/nils/Test-och-Automatisering-Data','add','data/'])
 subprocess.call(['git','-C','/home/nils/Test-och-Automatisering-Data','commit','-m','new data'])
 subprocess.call(['git','-C','/home/nils/Test-och-Automatisering-Data','push'])
