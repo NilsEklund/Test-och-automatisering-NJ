8c3ec483d0d676994f59e4ff65543fd557977873
Jasmin <jasvel0502@skola.goteborg.se>
2024-10-04 11:06:12 +0200

    SUPERKOMMENTERAD KOD

--- pwm.py
+++ pwm.py
@@ -1,19 +1,34 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import numpy as np
+import time
 
-pwm_pin = 13                # PWM pin connected to LED
-GPIO.setwarnings(False)            #disable warnings
-GPIO.setmode(GPIO.BOARD)        #set pin numbering system
-GPIO.setup(pwm_pin,GPIO.OUT)
-pi_pwm = GPIO.PWM(pwm_pin,1000)        #create PWM instance with frequency
-pi_pwm.start(0)                #start PWM of required Duty Cycle 
-while True:
-    for duty in range(0,101,1):
-        pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
-        sleep(0.01)
-    sleep(0.5)
-    
-    for duty in range(100,-1,-1):
-        pi_pwm.ChangeDutyCycle(duty)
-        sleep(0.01)
-    sleep(0.5)
+# GPIO setup
+GPIO.setmode(GPIO.BCM)  # Använd BCM-nummer
+pin = 13  # GPIO 13 motsvarar fysiska pinne 33
+GPIO.setup(pin, GPIO.OUT)
+
+# PWM setup
+pwm_frequency = 1000  # PWM frekvens i Hz
+pwm = GPIO.PWM(pin, pwm_frequency)  # Initiera PWM på pinne 13
+pwm.start(0)  # Starta PWM med 0% duty cycle
+
+# Parametrar
+sin_frequency = 50  # 50 Hz sinusvåg
+sampling_rate = pwm_frequency * 20  # Samplingsfrekvens
+duration = 15  # Kör programmet i 15 sekunder
+
+# Generera sinusvågen
+t = np.linspace(0, duration, int(sampling_rate * duration), endpoint=False)
+sin_wave = 0.5 * (1 + np.sin(2 * np.pi * sin_frequency * t))  # Normalisera mellan 0 och 1
+
+# Skicka ut PWM signalen
+try:
+    for value in sin_wave:
+        duty_cycle = value * 100  # Konvertera till % (mellan 0 och 100)
+        pwm.ChangeDutyCycle(duty_cycle)
+        time.sleep(1 / sampling_rate)  # Vänta tills nästa sampling
+
+finally:
+    pwm.stop()  # Stanna PWM
+    GPIO.cleanup()  # Rensa GPIO inställningar
+
